8ea0375af381d9faf21e4f2de0f3e8972b2b06a0
taukeer liaqat <taukeer.liaqat@gmail.com>
2017-07-19 08:47:49 +0500

    initial commit

---
+++ src/Cats.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+namespace Foo\Bar\Example;
+class Cats
+{
+    protected $names;
+    public function __construct(array $names)
+    {
+        if (count($names) === 0) {
+            throw new \InvalidArgumentException('There must be a choice of at least 1 cat.');
+        }
+        $this->names = array_values($names);
+    }
+    public function generate()
+    {
+        $choice = random_int(0, count($this->names) - 1);
+        return $this->names[$choice];
+    }
+}
